cb228490fb57f4559626e9dd095a1a35cb51d0ea
Brian Cantrell <bcantrell@gmail.com>
2020-08-02 09:14:20 -0400

    Add tests for organizing images into directories

--- organizer.rb
+++ organizer.rb
@@ -1,16 +1,53 @@
+require 'exifr/jpeg'
+require 'pry'
+
 class Organizer
-  def init(source_directory, destination_directory)
-    @source_directory = source_directory
-    @destination_directory = destination_directory
+  def initialize(source_directory, destination_directory)
+    @source_directory = ensure_trailing_slash(source_directory)
+    @destination_directory = ensure_trailing_slash(destination_directory)
   end
 
-  def organize
-    @source_directory.each do |image|
-      puts image
-      puts EXIFR::JPEG.new(image).width.to_s
-
-      # Dir["spec/fixtures/images/*"]
+  def ensure_trailing_slash(directory_path)
+    if directory_path[-1] != "/"
+      directory_path + "/"
+    else
+      directory_path
     end
   end
 
+  def organize
+    Dir.children(@source_directory).each do |item|
+      next if File.directory?(item)
+
+      source_photo_path = File.join(@source_directory, item)
+      photo_year, photo_month = photo_time_parts(source_photo_path)
+
+      if photo_year.nil?
+        target_directory = @destination_directory + "0000-no-exif"
+      else
+        target_directory = @destination_directory + File.join(photo_year, photo_month)
+      end
+
+      FileUtils.mkdir_p(target_directory)
+
+      if move?
+        FileUtils.mv(source_photo_path, File.join(target_directory, item))
+      else
+        FileUtils.cp(source_photo_path, File.join(target_directory, item))
+      end
+    end
+  end
+
+  def move?
+    false
+  end
+
+  def photo_time_parts(photo_path)
+      photo_date_time = EXIFR::JPEG.new(photo_path).date_time
+      return nil if photo_date_time.nil?
+
+      photo_year = photo_date_time.strftime("%Y")
+      photo_month = photo_date_time.strftime("%m")
+      return [photo_year, photo_month]
+  end
 end
--- spec/organizer_spec.rb
+++ spec/organizer_spec.rb
@@ -1,17 +1,17 @@
 require "rspec"
+require_relative "../organizer"
 
 describe "organizer" do
-  let(:source_directory) { "fixtures/source_temp" }
-  let(:destination_directory) { "fixtures/destination_temp" }
+  let(:source_directory) { "spec/fixtures/source_temp" }
+  let(:destination_directory) { "spec/fixtures/destination_temp" }
 
-  subject { Organizer.new(Dir[source_directory], Dir[destination_directory]) }
+  subject { Organizer.new(source_directory, destination_directory) }
 
   before do
     FileUtils.mkdir source_directory
     FileUtils.mkdir destination_directory
 
-    FileUtils.cp_r Dir["fixtures/images/*"], Dir[source_directory]
-    # Dir.entries(File.dirname(__FILE__) + "/spec/fixtures/images")
+    FileUtils.cp_r Dir["spec/fixtures/images/*"], Dir[source_directory]
   end
 
   after do
@@ -19,12 +19,28 @@ describe "organizer" do
     FileUtils.rm_rf destination_directory
   end
 
-  it "creates year and month subdirectories in destination directory" do
+  it "organizes photos into year/month directory based on exif datetime" do
     subject.organize
+
+    expect(FileUtils.compare_file(
+      File.join(source_directory, "Canon_40D.jpg"), 
+      File.join(destination_directory, "2008", "07", "Canon_40D.jpg"))).to be_truthy
+
+    expect(FileUtils.compare_file(
+      File.join(source_directory, "BlueSquare.jpg"), 
+      File.join(destination_directory, "2005", "09", "BlueSquare.jpg"))).to be_truthy
+
+    expect(FileUtils.compare_file(
+      File.join(source_directory, "canon-ixus.jpg"), 
+      File.join(destination_directory, "2001", "06", "canon-ixus.jpg"))).to be_truthy
   end
 
-  it "organizes photos into the correct directory" do
-    expect(true).to be_truthy
+  it "puts files without exif data in 0000-no-exif directory" do
+    subject.organize
+
+    expect(FileUtils.compare_file(
+      File.join(source_directory, "00-45.jpeg"), 
+      File.join(destination_directory, "0000-no-exif", "00-45.jpeg"))).to be_truthy
   end
 
   it "deletes organized images from source directory" do
